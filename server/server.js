db4f97be41ec4c46f1ebbab162e9e6be10cacc73
M Homam <mharjomandi@yahoo.com>
2017-07-01 02:38:07 -0400

    Ad Socket connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
+// javascript for socket.io: localhost:3000/socket.io/socket.io.js
+
+
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });
